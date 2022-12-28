842b6c2a64c37d5c11d4a15c61a8443f653505c3
Andreas Thomas <dev@chronark.com>
2022-12-28 13:17:35 +0100

    feat: fix build

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -13,50 +13,50 @@ import zlib from "zlib";
 
 // Mapping from Node's `process.arch` to Golang's `$GOARCH`
 const ARCH_MAPPING = {
-  x64: "amd64",
-  arm64: "arm64",
+    x64: "amd64",
+    arm64: "arm64",
 };
 
 // Mapping between Node's `process.platform` to Golang's
 const PLATFORM_MAPPING = {
-  darwin: "darwin",
-  linux: "linux",
-  win32: "windows",
+    darwin: "darwin",
+    linux: "linux",
+    win32: "windows",
 };
 
 // TODO: import pkg from "../package.json" assert { type: "json" };
 const readPackageJson = async () => {
-  const packageJsonPath = path.join(".", "package.json");
-  const contents = await fs.promises.readFile(packageJsonPath);
-  return JSON.parse(contents);
+    const packageJsonPath = path.join(".", "package.json");
+    const contents = await fs.promises.readFile(packageJsonPath);
+    return JSON.parse(contents);
 };
 
 const parsePackageJson = (packageJson) => {
-  const arch = ARCH_MAPPING[process.arch];
-  if (!arch) {
-    throw Error(
-      `Installation is not supported for this architecture: ${process.arch}`
-    );
-  }
+    const arch = ARCH_MAPPING[process.arch];
+    if (!arch) {
+        throw Error(
+            `Installation is not supported for this architecture: ${process.arch}`
+        );
+    }
 
-  const platform = PLATFORM_MAPPING[process.platform];
-  if (!platform) {
-    throw Error(
-      `Installation is not supported for this platform: ${process.platform}`
-    );
-  }
+    const platform = PLATFORM_MAPPING[process.platform];
+    if (!platform) {
+        throw Error(
+            `Installation is not supported for this platform: ${process.platform}`
+        );
+    }
 
-  // Build the download url from package.json
-  const version = packageJson.version;
-  const repo = packageJson.repository;
-  const url = `https://github.com/${repo}/releases/download/v${version}/cli_${version}_${platform}_${arch}.tar.gz`;
+    // Build the download url from package.json
+    const version = packageJson.version;
+    const repo = packageJson.repository;
+    const url = `https://github.com/${repo}/releases/download/v${version}/cli_${version}_${platform}_${arch}.tar.gz`;
 
-  let binPath = path.join("bin", "durable");
-  if (platform == "windows") {
-    binPath += ".exe";
-  }
+    let binPath = path.join("bin", "durable");
+    if (platform === "windows") {
+        binPath += ".exe";
+    }
 
-  return { binPath, url };
+    return { binPath, url };
 };
 
 const errGlobal = "Installing Durable CLI as a global module is not supported.";
@@ -69,39 +69,43 @@ const errGlobal = "Installing Durable CLI as a global module is not supported.";
  *  See: https://docs.npmjs.com/files/package.json#bin
  */
 async function main() {
-  const yarnGlobal = JSON.parse(
-    process.env.npm_config_argv || "{}"
-  ).original?.includes("global");
-  if (process.env.npm_config_global || yarnGlobal) {
-    throw errGlobal;
-  }
+    const yarnGlobal = JSON.parse(
+        process.env.npm_config_argv || "{}"
+    ).original?.includes("global");
+    if (process.env.npm_config_global || yarnGlobal) {
+        throw errGlobal;
+    }
 
-  const pkg = await readPackageJson();
-  const { binPath, url } = parsePackageJson(pkg);
-  const binDir = path.dirname(binPath);
-  await fs.promises.mkdir(binDir, { recursive: true });
+    const pkg = await readPackageJson();
+    const { binPath, url } = parsePackageJson(pkg);
+    const binDir = path.dirname(binPath);
+    await fs.promises.mkdir(binDir, { recursive: true });
 
-  // First we will Un-GZip, then we will untar.
-  const ungz = zlib.createGunzip();
-  const binName = path.basename(binPath);
-  const untar = tar.x({ cwd: binDir }, [binName]);
+    // First we will Un-GZip, then we will untar.
+    const ungz = zlib.createGunzip();
+    const binName = path.basename(binPath);
+    const untar = tar.extract({ path: binName });
 
-  console.info("Downloading", url);
-  const resp = await fetch(url);
-  resp.body.pipe(ungz).pipe(untar);
-  await new Promise((resolve, reject) => {
-    untar.on("error", reject);
-    untar.on("end", () => resolve());
-  });
 
-  // Link the binaries in postinstall to support yarn
-  await binLinks({
-    path: path.resolve("."),
-    pkg: { ...pkg, bin: { [pkg.name]: binPath } },
-  });
+    console.info("Downloading", url2);
+    const res = await fetch(url2);
+    const fileStream = fs.createWriteStream(binName);
 
-  // TODO: verify checksums
-  console.info("Installed Durable CLI successfully");
+
+    // Link the binaries in postinstall to support yarn
+    await binLinks({
+        path: path.resolve("."),
+        pkg: { ...pkg, bin: { [pkg.name]: binPath } },
+    });
+    await new Promise((resolve, reject) => {
+        res.body.pipe(fileStream);
+        res.body.on("error", reject);
+        fileStream.on("finish", resolve);
+    });
+
+
+    // TODO: verify checksums
+    console.info("Installed Durable CLI successfully");
 }
 
 await main();
