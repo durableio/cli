756038481f18433ac3041410389bb97f24c096fe
Andreas Thomas <dev@chronark.com>
2022-12-28 12:16:44 +0100

    feat: remove comment

--- cmd/root.go
+++ cmd/root.go
@@ -28,8 +28,6 @@ to quickly create a Cobra application.`,
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
